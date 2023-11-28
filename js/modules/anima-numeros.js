1ce0eaab5ec3ff18e1a6c7a2bb3f284cd6658c88
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2023-11-28 14:34:06 -0300

    refatoração de anima numeros

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,25 +1,3 @@
-// export default class AnimaNumeros {
-//   constructor(numeros) {
-
-//   }
-//   const numeros = document.querySelectorAll('[data-numero]');
-
-//   numeros.forEach((numero) => {
-//     const total = +numero.innerText;
-//     const incremento = Math.floor(total / 100);
-
-//     let start = 0;
-//     const timer = setInterval(() => {
-//       start += incremento;
-//       numero.innerText = start;
-//       if (start > total) {
-//         numero.innerText = total;
-//         clearInterval(timer);
-//       }
-//     }, 25 * Math.random());
-//   });
-// }
-
 export default class AnimaNumeros {
   constructor(numeros, observerTarget, observerClass) {
     this.numeros = document.querySelectorAll(numeros);
